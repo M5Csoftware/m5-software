1db80d47f155303413b7895f1a7ff24fdd6e5031
Altaf Raza <altafraza.07work@gmail.com>
2026-04-16 17:12:21 +0530

    Delete check_db.js

--- check_db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const url = process.env.MONGODB_URI || "mongodb+srv://developerAltaf:XoowF2o7i5XN9b9w@cluster0.h1oemoz.mongodb.net/M5-logistic?retryWrites=true&w=majority";
-mongoose.connect(url).then(async () => {
-    console.log("Connected");
-    const shipments = await mongoose.connection.collection('shipments').find({}).limit(5).toArray();
-    console.log(shipments.map(s => ({ date: s.date, createdAt: s.createdAt, origin: s.origin, destination: s.destination })));
-    process.exit();
-}).catch(console.error);
