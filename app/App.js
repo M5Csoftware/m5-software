1206f2053a0a6915765edc276f14c9bf4576379a
Altaf Raza <altafraza.07work@gmail.com>
2026-05-12 15:10:33 +0530

    Update App.js

--- app/App.js
+++ app/App.js
@@ -14,6 +14,7 @@ import { defaultEventCodes } from "./lib/eventCodes";
 import axios from "axios";
 import Sidebar from "./components/Sidebar";
 import Tabs from "./components/Tabs";
+import ChatSlider from "./components/ChatSlider";
 
 const PageLoader = () => (
   <div className="flex justify-center items-center h-full">
@@ -620,6 +621,7 @@ const App = () => {
           })}
         </div>
       </div>
+      <ChatSlider />
     </main>
   );
 };
